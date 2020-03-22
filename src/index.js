7ec92cabedc2b0d0218f34c5912365bbdb313ddb
Vadym Kostiuk <vadim.dk81@gmail.com>
2020-03-22 13:16:45 +0200

    feat: pass tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0;
+    } else {
+        let len = array.length;
+        if (len === 0 || array === undefined) {
+            return 0;
+        } else {
+            let min = Infinity;
+            while (len--) {
+                if (array[len] < min) {
+                    min = array[len]
+                }
+            }
+            return min;
+        }
+    }
 
-exports.min = function min (array) {
-  return 0;
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0;
+    } else {
+        let len = array.length;
+        if (len === 0) {
+            return 0;
+        } else {
+            let max = -Infinity;
+            while (len--) {
+                if (array[len] > max) {
+                    max = array[len]
+                }
+            }
+            return max;
+        }
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    } else {
+        let len = array.length;
+        if (len === 0) {
+            return 0;
+        } else {
+            let avg = 0;
+            let sum = array.reduce(function (a, b) {
+                return a + b;
+
+            }, 0);
+
+
+
+            avg = sum / len;
+
+            return avg;
+        }
+
+    }
+
 }
